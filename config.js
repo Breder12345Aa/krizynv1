7bb49c9d174e1b70b419871648fbe58395ab8d33
Breder12345Aa <82520506+Breder12345Aa@users.noreply.github.com>
2022-05-09 20:36:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ gc1 = 'https://instagram.com/mhdfakri_'
 gc2 = 'https://m.youtube.com/channel/UCzJakcNVDiAVnvuFJ4YAoZA' // Tolong jangan lupa untuk langsung subscribe ya
 gc3 = 'https://vt.tiktok.com/ZSeCJHbfA/'
 global.linkGC = ['https://chat.whatsapp.com/Eicqi0wT6Ia6QADH4Ukef9'] // ganti jadi group lu
-global.owner = ['62895328590064','62895327934887','16477944776','6289688069444','62895322095252'] // Put your number here //owner eval
-global.kontak = ['6289518487462','16477944776','62895328590064','62895327934887'] //Ketika ada yang ngetik #owner
+global.owner = ['6281360107561','62895322095252'] // Put your number here //owner eval
+global.kontak = ['6281360107561','62895322095252'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['62895328590064','62895327934887','16477944776','6289688069444','62895322095252'] // Premium user has unlimited limit
+global.prems = ['6281360107561'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
